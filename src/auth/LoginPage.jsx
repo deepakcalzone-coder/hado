4d3dd66d303c58d857f652e0c8ab54f779844afd
deepakcalzone-coder <deepakcalzone@gmail.com>
2026-07-09 10:57:22 +0530

    Update LoginPage.jsx

--- src/auth/LoginPage.jsx
+++ src/auth/LoginPage.jsx
@@ -15,7 +15,7 @@ export default function LoginPage() {
     setError("");
 
     try {
-      const res = await fetch("http://103.181.108.248/:5005/api/login", {
+      const res = await fetch("http://103.181.108.248/api/login", {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
